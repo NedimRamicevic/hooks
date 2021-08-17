b5eda423b8cc62da073570f6ed8c871071901a94
Nedim <mr.ramicevic@gmail.com>
2021-08-17 19:42:28 +0300

    set

--- src/Timer.js
+++ src/Timer.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 export default function Timer() {
   const [time, setTime] = useState(0);
-  useEffect(() => {
-  setInterval(() => {
-    setTime((prev) => prev + 1);
-  }, 1000);
-});
+  
+useEffect(() => {
+    setInterval(
+        () => {
+            setTime(x => x + 1 )
+        }
+        ,1000)
+})
+
   return (
     <>
       <h1>Time: {time}</h1>
